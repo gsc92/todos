ec5e218dfe3219b06a7a174e1a0b9ba2b4863b3b
Gökçe Sarıçiyil <goekce.sariciyil@blackhan.com>
2015-12-30 02:43:35 +0200

    clear input

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-	$('input#todo').keypress(function(ev) {
+	$('input#todo').keyup(function(ev) {
 		var value = $(ev.target).val();
 		if (ev.keyCode == 13) {
 			$('ul#todo-list').append('<li>' + value + '</li>');
+			ev.preventDefault();
+        $(this).val('');
+        return false;
 		}
 	});
 });
