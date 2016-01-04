c8e0533403f979af322d1b44c1d3c420c9057b32
Gökçe Sarıçiyil <goekce.sariciyil@blackhan.com>
2016-01-04 14:14:08 +0200

    added checkbox

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
 	$('input#todo').keyup(function(ev) {
 		var value = $(ev.target).val();
 		if (ev.keyCode == 13) {
+			$('ul#todo-list').append('<input type="checkbox" id="checkbox" style="color:white"/>');
 			$('ul#todo-list').append('<li>' + value + '</li>');
-			ev.preventDefault();
+			
+			ev.preventDefault();			
         $(this).val('');
         return false;
-		}
+		}		
 	});
 });
