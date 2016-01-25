23dc65b8eec9c2f28b9fb4af1bf275ed0e13dd0d
Gökçe Sarıçiyil <goekce.sariciyil@blackhan.com>
2016-01-25 15:32:54 +0200

    fixed checkbox toggle

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-$(document).ready(function(){
-	$('input#new-todo').keyup(function(ev) {
+$(document).ready(function () {
+	$('input#new-todo').keyup(function (ev) {
 		var value = $(ev.target).val();
 		var $ul = $('ul#todo-list');
 		if (ev.keyCode == 13) {
-			$ul.append('<div class="view">' + '<li>' + '<input type="checkbox" class="toggle"/>'+'<label>'+value+'</label>'+'<button class="destroy"></button>'+'</li>'+'</div>');
+			$ul.append(
+				'<li>' + 
+					'<input type="checkbox" class="toggle"/>' +
+					'<label>' + value + '</label>' +
+					'<button class="destroy"></button>' +
+				'</li>' 
+			);
+			
 			var $li_list = $ul.find('>li');
 			if ($li_list.length < 0){
 				$('#toggle-all').hide();
 			} else {
 				$('#toggle-all').show();
 			}
+			$(".toggle").off('click').on('click', function (ev) {
+				var $input = $(ev.target), 
+					$label = $input.siblings('label');
+				var is_checked = $input.prop('checked');
+				if (is_checked){
+					$label.addClass('completed');
+				} else {
+					$label.removeClass('completed');
+				}
+			});
 			ev.preventDefault();			
-    $(this).val('');
-    return false;
-	}		
-	});
-	$("#toggle").on('click', function(ev){
-		var is_checked = $(ev.target).prop('checked');
-		if($('.toggle').is(':checked')){
-			return('<strike><ul></ul></strike>');
-		}
+			$(this).val('');
+			return false;
+		}		
 	});
 	
 });
